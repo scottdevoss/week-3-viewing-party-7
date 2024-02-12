bf675a020a69202dcd4ddc80742764a3c031e92b
Scott DeVoss <scottdevoss@yahoo.com>
2024-02-12 14:18:35 -0700

    progress

--- spec/features/users/user_can_register_spec.rb
+++ spec/features/users/user_can_register_spec.rb
@@ -1,13 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "Registration Happy Path" do
-  xit "Registers a User" do
+  it "Registers a User" do
     visit "/register"
 
-    fill_in :username, with: "happygilmore"
-    fill_in :password, with: "golf123"
-    fill_in :password_confirmation, with: "golf123"
+    fill_in :user_name, with: "Happy Gilmore"
+    fill_in :user_email, with: "happygilmore@gmail.com"
+    fill_in :user_password, with: "golf123"
+    fill_in :user_password_confirmation, with: "golf123"
 
-    expect(current_path).to eq("/users")
+    expect(current_path).to eq("/users/#{User.last.id}")
   end
 end
