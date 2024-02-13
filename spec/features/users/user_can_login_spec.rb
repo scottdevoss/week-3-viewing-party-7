60807e36b7e3b297e3b8cc3aa2f99f435f6bc71b
Scott DeVoss <scottdevoss@yahoo.com>
2024-02-13 14:46:00 -0700

    completed US5

--- spec/features/users/user_can_login_spec.rb
+++ spec/features/users/user_can_login_spec.rb
@@ -58,6 +58,7 @@ RSpec.describe "User Log In" do
     # And I see that the Log Out link has changed back to a Log In link
     # And I still see the Create an Account button. 
     @user = User.create!(name: "Peyton Manning", email: "peyton@example.com", password: "football", password_confirmation: "football")
+    @user2 = User.create!(name: "Patrick Mahomes", email: "mahomies@example.com", password: "football123", password_confirmation: "football123")
     
     visit "/"
 
@@ -88,8 +89,42 @@ RSpec.describe "User Log In" do
   end
 
   it "logged out users see limited info on landing page" do
+    # As a visitor
+    # When I visit the landing page
+    # I do not see the section of the page that lists existing users
+
     visit "/"
 
     expect(page).to_not have_content("Existing Users:")
+  end
+
+  it "Logged-in users no longer see links on landing page" do
+    # As a logged-in user
+    # When I visit the landing page 
+    # The list of existing users is no longer a link to their show pages
+    # But just a list of email addresses
+
+    @user = User.create!(name: "Peyton Manning", email: "peyton@example.com", password: "football", password_confirmation: "football")
+    @user2 = User.create!(name: "Patrick Mahomes", email: "mahomies@example.com", password: "football123", password_confirmation: "football123")
+
+    visit "/"
+
+    click_on "Log In"
+
+    expect(current_path).to eq("/login")
+
+    fill_in :email, with: "peyton@example.com"
+    fill_in :password, with: "football"
+
+    click_button "Log In"
+
+    expect(current_path).to eq("/users/#{@user.id}")
+
+    visit "/"
+
+    expect(page).to have_content("mahomies@example.com")
+    expect(page).to have_content("peyton@example.com")
+    expect(page).to_not have_link("mahomies@example.com")
+    expect(page).to_not have_link("peyton@example.com")
   end
 end 
